98e597dc007d19bee8443bac9f3ca0f0c9de7f7a
cxjyxx_me <498731903@qq.com>
2017-11-03 19:53:02 +0800

    new config

--- config.py
+++ config.py
@@ -1,36 +1,7 @@
 import cv2
 from PIL import Image
 import numpy as np
-
-height = 288
-width = 512
-batch_size = 10
-initial_learning_rate = 2e-4
-theta_mul = 400
-regu_mul = 30
-img_mul = 1
-temp_mul = 30
-black_mul = 3000000
-training_iter = 70000
-step_size = 30000
-train_data_size = 4000
-test_data_size = 600
-crop_rate = 0.5
-before_ch = 7
-after_ch = 7
-tot_ch = before_ch + after_ch + 1
-test_batches = 10
-random_crop_rate = 0.9
-disp_freq = 50
-test_freq = 500
-save_freq = 1000
-no_theta_iter = 1000000
-do_temp_loss_iter = 5000
-do_theta_10_iter = -1
-do_black_loss_iter = 300
-tfrecord_item_num = 10
-log_dir = 'log/28/'
-model_dir = 'models/28/'
+from configs.c28 import *
 
 def cvt_img2train(img, crop_rate = 1):
     img = Image.fromarray(cv2.cvtColor(img,cv2.COLOR_BGR2GRAY))
