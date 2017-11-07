919ac2e77d3ef6e87d978ed364139a8df2dc5b16
cxjyxx_me <498731903@qq.com>
2017-11-07 19:29:45 +0800

    39

--- gen_data_flow.py
+++ gen_data_flow.py
@@ -8,6 +8,7 @@ import sys
 import struct
 
 data_names = ["train", "test"]
+data_path = "data4/"
 for dn in data_names:
     list_f = open('data_video/' + dn + '_list', 'r+')
     temp = list_f.read()
@@ -16,7 +17,7 @@ for dn in data_names:
     record_num = 0
     file_num = 0
     file_list = str(file_num) + ".tfrecords"
-    writer = tf.python_io.TFRecordWriter("data3/" + dn + "/" + str(file_num) + ".tfrecords")
+    writer = tf.python_io.TFRecordWriter(data_path + dn + "/" + str(file_num) + ".tfrecords")
 
     for video_name in video_list:
         if (video_name == ""):
@@ -81,7 +82,7 @@ for dn in data_names:
                 file_num += 1
                 writer.close()
                 file_list += " " + str(file_num) + ".tfrecords"
-                writer = tf.python_io.TFRecordWriter("data3/" + dn + "/" + str(file_num) + ".tfrecords")
+                writer = tf.python_io.TFRecordWriter(data_path + dn + "/" + str(file_num) + ".tfrecords")
 
             ret, frame_stable = stable_cap.read()  
             ret, frame_unstable = unstable_cap.read()
@@ -94,6 +95,6 @@ for dn in data_names:
         stable_cap.release()  
         unstable_cap.release()  
     writer.close()
-    file_object = open("data3/" + dn + "/list.txt", 'w')
+    file_object = open(data_path + dn + "/list.txt", 'w')
     file_object.write(file_list)
     file_object.close( )
